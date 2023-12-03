ac0fdbf31d7c5cbd60eb779634d90098c847c1d9
Bernardo Facchi <cataratasdolly@outlook.com>
2023-12-03 16:52:07 -0300

    Added whip sound

--- src/main/java/net/whips/Whip.java
+++ src/main/java/net/whips/Whip.java
@@ -1,11 +1,24 @@
 package net.whips;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundCategory;
+
+import static net.whips.Whips.WHIP_SOUND_EVENT;
+
 
 public class Whip extends SwordItem {
     public Whip(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        attacker.getWorld().playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), WHIP_SOUND_EVENT, SoundCategory.HOSTILE, 1.5f, 1.0F / (attacker.getWorld().random.nextFloat() * 0.4F + 1.2F) + 0.5F);
+
+        return super.postHit(stack, target, attacker);
     }
 }
--- src/main/java/net/whips/Whips.java
+++ src/main/java/net/whips/Whips.java
@@ -6,13 +6,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public class Whips implements ModInitializer {
     public static final Item WHIP = new Whip(ToolMaterials.WOOD, 2, 1.5f, new FabricItemSettings());
 
+    public static final Identifier WHIP_SOUND = new Identifier("whips:whip_sound");
+    public static SoundEvent WHIP_SOUND_EVENT = SoundEvent.of(WHIP_SOUND);
+
     @Override
     public void onInitialize() {
         Registry.register(Registries.ITEM, new Identifier("whips", "whip"), WHIP);
+        Registry.register(Registries.SOUND_EVENT, WHIP_SOUND, WHIP_SOUND_EVENT);
     }
 }
