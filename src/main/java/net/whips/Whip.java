c992ef8432cfea7a5a4cde5c0a1a7e3fb0745e3b
Bernardo Facchi <cataratasdolly@outlook.com>
2023-12-03 17:26:56 -0300

    Villagers decrease price now

--- src/main/java/net/whips/Whip.java
+++ src/main/java/net/whips/Whip.java
@@ -1,11 +1,13 @@
 package net.whips;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.village.VillageGossipType;
 
 import static net.whips.Whips.WHIP_SOUND_EVENT;
 
@@ -18,6 +20,11 @@ public class Whip extends SwordItem {
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         attacker.getWorld().playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), WHIP_SOUND_EVENT, SoundCategory.HOSTILE, 5f, 1.0F / (attacker.getWorld().random.nextFloat() * 0.4F + 1.2F) + 0.5F);
+
+        if (target instanceof VillagerEntity) {
+            ((VillagerEntity) target).getGossip().removeGossip(attacker.getUuid(), VillageGossipType.MINOR_NEGATIVE, 25);
+            ((VillagerEntity) target).getGossip().startGossip(attacker.getUuid(), VillageGossipType.MINOR_POSITIVE, 25);
+        }
 
         return super.postHit(stack, target, attacker);
     }
